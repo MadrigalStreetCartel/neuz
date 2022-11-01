29bae6c0a9df1b32a4300a96d27d0b2b9325528e
Slyker <ramzy694@gmail.com>
2022-11-01 15:53:26 +0100

    Update StopWatch.tsx

--- src/components/utils/StopWatch.tsx
+++ src/components/utils/StopWatch.tsx
@@ -8,15 +8,13 @@ class StopWatchValues {
     timer = 0
 
     constructor(timer: number) {
-        this.ms = ("0" + ((timer / 10) % 100)).slice(-2)
-        this.secs = ("0" + Math.floor((timer / 1000) % 60)).slice(-2)
-        this.mins = ("0" + Math.floor((timer / 60000) % 60)).slice(-2)
-        this.hours = ("0" + Math.floor((timer / 3600000) % 60)).slice(-2)
-        this.timer = timer
+        this.update(timer)
     }
+
     toString = () => {
         return `${this.hours}:${this.mins}:${this.secs}:${this.ms}:`
     }
+
     add = (watch: StopWatchValues) => {
         return new StopWatchValues(this.timer + watch.timer)
     }
