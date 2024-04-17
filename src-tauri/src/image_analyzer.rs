31508d7c343f281fd7f3365c303873004eff2490
Slyker <ramzy694@gmail.com>
2024-04-16 23:01:58 -0400

    Removes usge of par_iter ( makes it slower )

--- src-tauri/src/image_analyzer.rs
+++ src-tauri/src/image_analyzer.rs
@@ -1,22 +1,26 @@
 use std::collections::HashMap;
-use std::{sync::mpsc::sync_channel, time::Instant};
+use std::{ sync::mpsc::sync_channel, time::Instant };
 //use libscreenshot::shared::Area;
-use libscreenshot::{ImageBuffer, WindowCaptureProvider};
+use libscreenshot::{ ImageBuffer, WindowCaptureProvider };
 use rayon::iter::{
-    IntoParallelIterator, IntoParallelRefIterator, ParallelBridge, ParallelIterator,
+    ParallelBridge,
+    ParallelIterator,
 };
 use slog::Logger;
 use tauri::Window;
 
 use crate::client_state::state::ClientState;
 use crate::cloud_detection::{
-    CloudDetection, CloudDetectionConfig, CloudDetectionKind, CloudDetectionType,
+    CloudDetection,
+    CloudDetectionConfig,
+    CloudDetectionKind,
+    CloudDetectionType,
     CloudDetectionZone,
 };
 
 use crate::{
-    data::{Bounds, Point, PointCloud, Target},
-    platform::{IGNORE_AREA_BOTTOM, IGNORE_AREA_TOP},
+    data::{ Bounds, Point, PointCloud, Target },
+    platform::{ IGNORE_AREA_BOTTOM, IGNORE_AREA_TOP },
     utils::Timer,
 };
 
@@ -60,17 +64,37 @@ impl<'a> ImageAnalyzer<'a> {
     pub fn pixel_detection(&mut self, config: &Vec<CloudDetectionConfig>) {
         /* -> Vec<CloudDetection> */
         const DETECTION_BUFFER: usize = 4096;
-        let (snd, recv) =
-            sync_channel::<(&CloudDetectionZone, CloudDetectionType, Point)>(DETECTION_BUFFER);
+        let (snd, recv) = sync_channel::<(&CloudDetectionZone, CloudDetectionType, Point)>(
+            DETECTION_BUFFER
+        );
         let image = self.image.as_ref().unwrap();
 
-        let config = &config.iter().filter(|x| x.enabled).collect::<Vec<_>>();
+        let config = &config
+            .iter()
+            .filter(|x| x.enabled)
+            .collect::<Vec<_>>();
 
-        let mut max_x = config.iter().map(|x| x.bounds.w).max().unwrap_or(0);
-        let mut max_y = config.iter().map(|x| x.bounds.h).max().unwrap_or(0);
+        let mut max_x = config
+            .iter()
+            .map(|x| x.bounds.w)
+            .max()
+            .unwrap_or(0);
+        let mut max_y = config
+            .iter()
+            .map(|x| x.bounds.h)
+            .max()
+            .unwrap_or(0);
 
-        let min_x = config.iter().map(|x| x.bounds.x).min().unwrap_or(0);
-        let min_y = config.iter().map(|x| x.bounds.y).min().unwrap_or(0);
+        let min_x = config
+            .iter()
+            .map(|x| x.bounds.x)
+            .min()
+            .unwrap_or(0);
+        let min_y = config
+            .iter()
+            .map(|x| x.bounds.y)
+            .min()
+            .unwrap_or(0);
 
         let image_width = image.width();
         if max_x == 0 {
@@ -81,9 +105,7 @@ impl<'a> ImageAnalyzer<'a> {
         if max_y == 0 {
             max_y = image_height;
         }
-        let image_height = image_height
-            .checked_sub(IGNORE_AREA_BOTTOM)
-            .unwrap_or(image_height);
+        let image_height = image_height.checked_sub(IGNORE_AREA_BOTTOM).unwrap_or(image_height);
 
         image
             .enumerate_rows()
@@ -91,11 +113,12 @@ impl<'a> ImageAnalyzer<'a> {
             .for_each(move |(y, row)| {
                 // Skip this row if it's in an ignored area
                 #[allow(clippy::absurd_extreme_comparisons)] // not always 0 (macOS)
-                if y <= IGNORE_AREA_TOP
-                    || y > image_height
-                    || y > IGNORE_AREA_TOP + max_y
-                    || y > max_y
-                    || y < min_y
+                if
+                    y <= IGNORE_AREA_TOP ||
+                    y > image_height ||
+                    y > IGNORE_AREA_TOP + max_y ||
+                    y > max_y ||
+                    y < min_y
                 {
                     //println!("returning early on y: {}", y);
                     return;
@@ -114,10 +137,11 @@ impl<'a> ImageAnalyzer<'a> {
                             if let Some(cat_config) = _config.pixel_compare(&px.0) {
                                 #[allow(dropping_copy_types)]
                                 drop(
-                                    snd.try_send((&_config.zone, cat_config, Point::new(x, y)))
+                                    snd
+                                        .try_send((&_config.zone, cat_config, Point::new(x, y)))
                                         .map_err(|err| {
                                             eprintln!("Error sending data: {}", err);
-                                        }),
+                                        })
                                 );
                                 // Continue to next column
                                 continue 'outer;
@@ -129,8 +153,10 @@ impl<'a> ImageAnalyzer<'a> {
 
         // Receive points from channel
         let clouds = {
-            let mut clouds: HashMap<&CloudDetectionZone, HashMap<CloudDetectionType, PointCloud>> =
-                HashMap::default();
+            let mut clouds: HashMap<
+                &CloudDetectionZone,
+                HashMap<CloudDetectionType, PointCloud>
+            > = HashMap::default();
             for i in 0..config.len() {
                 let mut hashmap = HashMap::default();
                 let _configs: Vec<CloudDetectionType> = config[i].zone.get_types();
@@ -164,26 +190,30 @@ impl<'a> ImageAnalyzer<'a> {
         };
 
         self.client_state.update(&detected_clouds);
-        let result = detected_clouds.par_iter().find_map_first(move |cloud| {
+
+        let result = detected_clouds.iter().find_map(move |cloud| {
             match cloud.kind {
-                CloudDetectionType::Shape(t) => match t {
-                    CloudDetectionKind::TargetAggressive | CloudDetectionKind::TargetPassive => {
-                        let is_red = t == CloudDetectionKind::TargetAggressive;
-                        let target = cloud.process_target();
-                        if target.is_some() {
-                            return Some((target, is_red));
+                CloudDetectionType::Shape(t) =>
+                    match t {
+                        | CloudDetectionKind::TargetAggressive
+                        | CloudDetectionKind::TargetPassive => {
+                            let is_red = t == CloudDetectionKind::TargetAggressive;
+                            let target = cloud.process_target();
+                            if target.is_some() {
+                                return Some((target, is_red));
+                            }
                         }
+                        _ => {}
                     }
-                    _ => {}
-                },
                 _ => {}
             }
             None
         });
 
         let target = if let Some((target, _is_red)) = result {
-            self.client_state.target.distance =
-                Some(self.get_target_distance_to_player(target.unwrap()));
+            self.client_state.target.distance = Some(
+                self.get_target_distance_to_player(target.unwrap())
+            );
             target
         } else {
             self.client_state.target.distance = None;
@@ -217,15 +247,18 @@ impl<'a> ImageAnalyzer<'a> {
 
     pub fn identify_mobs(&self, clouds: &Vec<CloudDetection>) -> Vec<Target> {
         clouds
-            .par_iter()
-            .filter_map(|cloud| match cloud.kind {
-                CloudDetectionType::Text(t) => match t {
-                    CloudDetectionKind::MobAggressive | CloudDetectionKind::MobPassive => {
-                        Some(cloud.process_mobs())
-                    }
+            .iter()
+            .filter_map(|cloud| {
+                match cloud.kind {
+                    CloudDetectionType::Text(t) =>
+                        match t {
+                            CloudDetectionKind::MobAggressive | CloudDetectionKind::MobPassive => {
+                                Some(cloud.process_mobs())
+                            }
+                            _ => None,
+                        }
                     _ => None,
-                },
-                _ => None,
+                }
             })
             .flatten()
             .collect()
@@ -248,7 +281,7 @@ impl<'a> ImageAnalyzer<'a> {
         mobs: &'a [Target],
         avoid_list: Option<&Vec<(Bounds, Instant, u128)>>,
         max_distance: i32,
-        _logger: &Logger,
+        _logger: &Logger
     ) -> Option<&'a Target> {
         let _timer = Timer::start_new("find_closest_mob");
 
@@ -261,7 +294,7 @@ impl<'a> ImageAnalyzer<'a> {
 
         // Calculate 2D euclidian distances to player
         let mut distances: Vec<(&Target, i32)> = mobs
-            .into_par_iter()
+            .iter()
             .map(|mob| {
                 let distance = mob.get_target_distance_to(point);
                 (mob, distance)
@@ -273,18 +306,18 @@ impl<'a> ImageAnalyzer<'a> {
 
         // Remove mobs that are too far away
         distances = distances
-            .into_par_iter()
+            .into_iter()
             .filter(|&(_, distance)| distance <= max_distance)
             .collect();
 
         if let Some(avoided_bounds) = avoid_list {
             // Try finding closest mob that's not the mob to be avoided
-            let closest = distances.par_iter().find_first(|(mob, _distance)| {
+            let closest = distances.into_iter().find(|(mob, _distance)| {
                 //*distance > 55
                 let coords = mob.get_attack_coords();
                 let avoided_bounds = avoided_bounds
-                    .par_iter()
-                    .find_first(|avoided_item| !avoided_item.0.contains_point(&coords));
+                    .into_iter()
+                    .find(|avoided_item| !avoided_item.0.contains_point(&coords));
 
                 if let Some(_) = avoided_bounds {
                     true
