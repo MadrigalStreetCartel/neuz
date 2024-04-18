011016ecadd098fa08a98c830f49cd4a436589b7
Slyker <ramzy694@gmail.com>
2024-04-18 11:22:06 -0400

    Refactor eval.js and shared.rs for improved performance and debug methods

--- src-tauri/src/platform/eval.js
+++ src-tauri/src/platform/eval.js
@@ -5,18 +5,58 @@ const DEBUG = true && $env.DEBUG
 function addTargetMarker(color = 'red', x = 0, y = 0,) {
     if (!DEBUG) return
     const targetMarker = document.createElement('div')
-    const targetMarkerStyle = `position: fixed; width: 2px; height: 2px; background-color: ${color}; border-radius: 50%;z-index: 9999;left: ${x}px;top: ${y}px;`
+    const targetMarkerStyle = `position: fixed; width: 3px; height: 3px; background-color: ${color}; border-radius: 50%;z-index: 9999;left: ${x}px;top: ${y}px;`
     targetMarker.style = targetMarkerStyle
     document.body.appendChild(targetMarker)
 
     setTimeout(() => {
         targetMarker.remove()
     }, 1000)
+
+    return targetMarker
+}
+
+function drawBounds(x, y, w, h) {
+    if (true || !DEBUG) return
+    const targetMarker = document.createElement('div')
+    const targetMarkerStyle = `position: fixed; width: ${w}px; height: ${h}px; border: 1px solid violet;z-index: 9999;left: ${x}px;top: ${y}px;`
+    targetMarker.style = targetMarkerStyle
+    document.body.appendChild(targetMarker)
+
+    // adds a div on top and display width and height
+    const targetMarkerText = document.createElement('div')
+    const targetMarkerTextStyle = `position: fixed; color: violet; z-index: 9999; left: ${x}px; top: ${(y - h) - 10}px;`
+    targetMarkerText.style = targetMarkerTextStyle
+    targetMarkerText.innerHTML = `${w}x${h}`
+    document.body.appendChild(targetMarkerText)
+
+
+
+    setTimeout(() => {
+        targetMarkerText.remove()
+        targetMarker.remove()
+
+    }, 1000)
+    return targetMarker
 }
 
 function isMob() {
     return document.body.style.cursor.indexOf('curattack') > 0
 }
+let try_check = false
+let coords = { x: 0, y: 0 }
+let tried_target = null
+setInterval(() => {
+    if (try_check && isMob()) {
+        tried_target.remove()
+        mouseEvent('press', coords.x, coords.y)
+        addTargetMarker('green', coords.x, coords.y)
+        try_check = false
+        setTimeout(() => {
+            mouseEvent('move', 800 / 2, 600 / 2)
+        }, 50)
+    }
+},0)
 
 
 function dispatchEvent(event) {
@@ -60,19 +100,35 @@ function mouseEvent(type, x, y, { checkMob = false, delay = 20, duration } = {})
             break;
         case 'moveClick':
             dispatchEvent(new MouseEvent('mousemove', { clientX: x, clientY: y }))
+            tried_target = addTargetMarker('orange', x, y)
 
             if (checkMob) {
-                if (mob_clicker_interval) {
-                    clearInterval(mob_clicker_interval)
-                }
+                try_check = true
+                coords = { x, y }
+                setTimeout(() => {
+                    try_check = false
+                }, 33)
+                /*  if (mob_clicker_interval) {
+                     clearInterval(mob_clicker_interval)
+                 }
 
-                mob_clicker_interval = setInterval(() => {
-                    if (isMob()) {
-                        mouseEvent('press', x, y)
-                        addTargetMarker('green', x, y)
-                        clearInterval(mob_clicker_interval)
-                    }
-                }, 0)
+                 mob_clicker_interval = setInterval(() => {
+                     let to = setTimeout(() => {
+                         if (mob_clicker_interval) {
+                             clearInterval(mob_clicker_interval)
+                         }
+                     }, 10)
+                     if (isMob()) {
+                         tried_target.remove()
+                         mouseEvent('press', x, y)
+                         addTargetMarker('green', x, y)
+                         clearTimeout(to)
+                         clearInterval(mob_clicker_interval)
+                         setTimeout(() => {
+                             mouseEvent('move', 800 / 2, 600 / 2)
+                         }, 30)
+                     }
+                 }, 0) */
                 /*  checkMobTimeout = setTimeout(() => {
                      if (isMob()) {
                          dispatchEvent(new MouseEvent('mousedown', { clientX: x, clientY: y }))
